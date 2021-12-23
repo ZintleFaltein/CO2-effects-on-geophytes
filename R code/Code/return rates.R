72b0e53401ab1457a825eda4fd8481063c379142
ZintleFaltein <zintlefaltein.zf@gmail.com>
2021-12-23 22:19:22 +0200

    Updated return rates at glacial CO2

--- R code/Code/return rates.R
+++ R code/Code/return rates.R
@@ -6,16 +6,14 @@ library(gridExtra)
 library(tidyverse)
 library(readr)
 library(curl)
+library(reshape)
 
-returns <- read_delim("https://raw.githubusercontent.com/ZintleFaltein/CO2-effects-on-geophytes/master/R%20code/Data/Return%20rates%20by%20veg%20type.csv?token=AOMLEIDI7IBQR5ALPBK42CLBYTG7I", 
+returns <- read_delim("https://raw.githubusercontent.com/ZintleFaltein/CO2-effects-on-geophytes/master/R%20code/Data/Foraging%20return%20rates.csv?token=AOMLEIHYF2HMAOH6ZEDAGTLBYTLX6", 
                                        delim = ";", escape_double = FALSE, trim_ws = TRUE)
 
-returns
+View(returns)
 
-returns$Cal_per_hr = as.numeric(sub("," , ".", returns$Cal_per_hr))
-
-head(returns)
-
+#returns$Cal_per_hr = as.numeric(sub("," , ".", returns$Cal_per_hr))
 
 themed <- theme(panel.grid.minor = element_blank(),
                panel.grid.major = element_blank(),
@@ -24,7 +22,16 @@ themed <- theme(panel.grid.minor = element_blank(),
                panel.border=element_rect(fill=NA,colour="black"),
                strip.background=element_rect(fill="grey 80",colour="black"),
                strip.text.x = element_text(size = 7))
-returns %>%
+returns <- returns %>%
+  select(`Vegetation Type`, `BOUT ID`, `return(400)`, 
+         `return(300)`, `return(240)`, `return(180)`) 
+
+# melt the dataframe
+returns <- melt(returns, id.vars = c(`Vegetation Type`, `BOUT ID`),
+                measure.vars = c(`return(400)`,`return(300)`, 
+                                 `return(240)`, `return(180)`))
+
+return %>%
   filter(Cal_per_hr >= 1000) %>%
   #filter(`Vegetation Type` == c('Dune fynbos-thicket mos.', 'Sand fynbos', 'Riparian')) %>%
   ggplot(aes(x=CO2, y = Cal_per_hr, group = CO2)) + 
