422b2e1b7c33ab009609771a48757cd9d7a3d448
ZintleFaltein <zintlefaltein.zf@gmail.com>
2022-11-16 10:41:34 +0200

    Corrected Photosynthesis and gst plots"

--- Code/R code/Fit_aci.R
+++ Code/R code/Fit_aci.R
@@ -1,5 +1,5 @@
 #' This script takes in a dataframe with photosynthetic values
-#' and uses the plantecophys package to return the values of 
+#' and uses the plantecophys package to return the values of
 #' Jmax, Vcmax and Rd
 #' It also plots A-Ci curves for all 3 experiments
 
@@ -10,13 +10,22 @@ library(tidyverse)
 library(ggpmisc)
 
 # load data
-aci <- read_excel("C:/Users/User/Documents/GitHub/CO2-effects-on-geophytes/Data/ACi fitting that corresponds to R library plantecophys.xlsx")
-aci_plot <- read_excel("C:/Users/User/Documents/GitHub/CO2-effects-on-geophytes/Data/ACi fitting that corresponds to R library plantecophys.xlsx", 
-                       sheet = "ACI values for R-Exp1&2")
-aci_e3 <- read_excel("C:/Users/User/Documents/GitHub/CO2-effects-on-geophytes/Data/ACi fitting that corresponds to R library plantecophys.xlsx", 
-                      sheet = "ACI values for R-Exp3")
+aci <-
+  read_excel(
+    "C:/Users/User/Documents/GitHub/CO2-effects-on-geophytes/Data/ACi fitting that corresponds to R library plantecophys.xlsx"
+  )
+aci_plot <-
+  read_excel(
+    "C:/Users/User/Documents/GitHub/CO2-effects-on-geophytes/Data/ACi fitting that corresponds to R library plantecophys.xlsx",
+    sheet = "ACI values for R-Exp1&2"
+  )
+aci_e3 <-
+  read_excel(
+    "C:/Users/User/Documents/GitHub/CO2-effects-on-geophytes/Data/ACi fitting that corresponds to R library plantecophys.xlsx",
+    sheet = "ACI values for R-Exp3"
+  )
 
-# correct the data types for CO2, leaf temperature & photosynthesis  
+# correct the data types for CO2, leaf temperature & photosynthesis
 aci$CO2S <- as.numeric(sub("," , ".", aci$CO2S))
 aci$Ci <- as.numeric(sub("," , ".", aci$Ci))
 aci$Tleaf <- as.numeric(sub("," , ".", aci$Tleaf))
@@ -31,126 +40,151 @@ aci_e3$SE <- as.numeric(sub("," , ".", aci_e3$SE))
 aci_e3$LA <- as.factor(aci_e3$LA)
 
 # create a function to get Vcmax, Jmax and Rd values
-fits <- function(exp, LA, rep, co2, fitmethod='default'){
-  #' Take in a dataframe and fit the ACI curve
-  #' 
+fits <- function(exp, LA, rep, co2, fitmethod = 'default') {
+  #' Take in a data frame and fit the ACI curve
+  #'
   #' @param Expt  Experiment number
   #' @param LA Nutrient concentration
   #' @param Rep  Replicate
   #' @param GCO2 Growth [CO2] applied to the plants
   #' @param fitmethod Method to fit the ACi curve
   #' @return A and GS at growth CO2
-  data = aci%>%
-    filter(Expt==exp,
-           #Spp==as.factor(spp), 
-           LA==LA, 
-           Rep==rep,
-           GCO2==co2) %>%
+  data = aci %>%
+    filter(Expt == exp,
+           #Spp==as.factor(spp),
+           LA == LA,
+           Rep == rep,
+           GCO2 == co2) %>%
     fitaci(Tcorrect = FALSE, fitmethod = fitmethod)
-    #ci = Photosyn(Ca = co2)
-    return(data$Photosyn(Ca = co2)[2:3])
+  #ci = Photosyn(Ca = co2)
+  return(data$Photosyn(Ca = co2)[2:3])
   # f= fitaci(data, Tcorrect = FALSE)
   # f[2]
-}  
+}
 
 ###########################################################
 #                   plot A-Ci curves                      #
 ###########################################################
 
-# get the formula for the curve
+# get the formula for the A-ci curve
 formula <- y ~ poly(x, 4, raw = TRUE)
 
+# plot the A-ci curves for each experiment
 # Experiment 2
 aci_plot %>%
   filter(Ave >= 0,
          Spp == 'O.pes-caprae',
          CO2 != '400') %>%
-  ggplot(aes(x=Ci, y=Ave)) +
-  geom_point() + 
-  geom_errorbar(aes(ymin=Ave-SE, ymax=Ave+SE)) +
-  stat_smooth(method = "lm", formula = formula, se = F, size=0.7, colour = 'black') +
+  ggplot(aes(x = Ci, y = Ave)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = Ave - SE, ymax = Ave + SE)) +
+  stat_smooth(
+    method = "lm",
+    formula = formula,
+    se = F,
+    size = 0.7,
+    colour = 'black'
+  ) +
   #stat_poly_eq(
-   # aes(label =  paste(..eq.label.., ..adj.rr.label.., sep = "~~~~")),
-    #formula = formula) +
-  #facet_wrap(Spp~CO2, nrow = 1) +
-  facet_grid(rows = vars(Spp), cols = vars(CO2)) +
+  # aes(label =  paste(..eq.label.., ..adj.rr.label.., sep = "~~~~")),
+  #formula = formula) +
+  facet_wrap(Spp~CO2, nrow = 1) +
+ #facet_grid(rows = vars(Spp), cols = vars(CO2)) +
   theme +
-  labs(x = expression(Ci~~(mu~mol~mol^-1)), y = expression(A~~(mu~mol~m^-2~s^-1)))
+  labs(x = expression(Ci ~  ~ (mu ~ mol ~ mol ^ -1)), y = expression(A ~
+                                                                       ~ (mu ~ mol ~ m ^ -2 ~ s ^ -1)))
 
 # find the photosynthetic rate at Ci=Ca
-# loop through all the replicates to get A and GS
-for (i in 1:5){
+# loop through all the replicates to get A and GS using the function fits
+for (i in 1:5) {
   print(fits(2, 100, i, 180))
 }
 
-for (i in 1:5){
+for (i in 1:5) {
   print(fits(2, 100, i, 240))
 }
 
-for (i in 1:5){
+for (i in 1:5) {
   print(fits(2, 100, i, 300))
 }
 
-for (i in 1:3){
-  print(fits(2, 100, i, 300))
-}
+
+#for (i in 1:3) {
+ # print(fits(2, 100, i, 400))
+#}
 
 # Experiment 1
 aci_plot %>%
   filter(Ave >= 0,
          Spp == 'O.punctata') %>%
-  ggplot(aes(x=Ci, y=Ave)) +
-  geom_point(shape) + 
-  geom_errorbar(aes(ymin=Ave-SE, ymax=Ave+SE)) +
-  geom_smooth(method = 'lm', formula = formula, se = F, size=0.7, colour = 'black') +
+  ggplot(aes(x = Ci, y = Ave)) +
+  geom_point() +
+  geom_errorbar(aes(ymin = Ave - SE, ymax = Ave + SE)) +
+  geom_smooth(
+    method = 'lm',
+    formula = formula,
+    se = F,
+    size = 0.7,
+    colour = 'black'
+  ) +
   # stat_smooth(method = "lm", formula = formula) +
   # stat_poly_eq(
   #   aes(label =  paste(..eq.label.., ..adj.rr.label.., sep = "~~~~")),
   #   formula = formula
   # ) +
   #facet_wrap(LA~CO2) +
-  facet_wrap(Spp ~ LA+CO2) +
+  facet_wrap(Spp ~ LA + CO2) +
   theme +
-  labs(x = expression(Ci~~(mu~mol~mol^-1)), y = expression(A~~(mu~mol~m^-2~s^-1)))
+  labs(x = expression(Ci ~  ~ (mu ~ mol ~ mol ^ -1)), y = expression(A ~
+                                                                       ~ (mu ~ mol ~ m ^ -2 ~ s ^ -1)))
 
 # find the photosynthetic rate at Ci=Ca
 # loop through all the replicates to get A and GS
-for (i in 1:3){
+for (i in 1:3) {
   print(fits(1, 50, i, 180, 'bilinear'))
 }
 
-for (i in 1:3){
+for (i in 1:3) {
   print(fits(1, 50, i, 280))
 }
 
-for (i in 1:3){
+for (i in 1:3) {
   print(fits(1, 50, i, 400))
 }
 
-for (i in 1:3){
+for (i in 1:3) {
   print(fits(1, 100, i, 180, 'bilinear'))
 }
 
-for (i in 1:3){
+for (i in 1:3) {
   print(fits(1, 100, i, 280))
 }
 
-for (i in 1:3){
+for (i in 1:3) {
   print(fits(1, 100, i, 400))
 }
 # Experiment 3
 aci_e3 %>%
   filter(Ave >= 0) %>%
-  ggplot(aes(x=Ci, y=Ave)) +
+  ggplot(aes(x = Ci, y = Ave)) +
   geom_point() +
-  geom_errorbar(aes(ymin=Ave-SE, ymax=Ave+SE)) +
-  geom_smooth(method = 'lm', formula = formula, se = F, size=0.7, colour = 'black') +
+  geom_errorbar(aes(ymin = Ave - SE, ymax = Ave + SE)) +
+  geom_smooth(
+    method = 'lm',
+    formula = formula,
+    se = F,
+    size = 0.7,
+    colour = 'black'
+  ) +
   # stat_smooth(method = "lm", formula = formula) +
   # stat_poly_eq(
   #   aes(label =  paste(..eq.label.., ..adj.rr.label.., sep = "~~~~")),
   #   formula = formula
   # ) +
-  facet_wrap(~factor(LA, levels = c('100% Long Ashton', '70% Long Ashton', '50% Long Ashton'))) +
+  facet_wrap( ~ factor(
+    LA,
+    levels = c('100% Long Ashton', '70% Long Ashton', '50% Long Ashton')
+  )) +
   theme + theme(strip.text.x = element_text(size = 10)) +
-  labs(x = expression(Ci~~(mu~mol~mol^-1)), y = expression(A~~(mu~mol~m^-2~s^-1)))
-
+  labs(x = expression(Ci ~  ~ (mu ~ mol ~ mol ^ -1)), y = expression(A ~
+                                                                       ~ (mu ~ mol ~ m ^ -2 ~ s ^ -1)))
