6c6b38462a16b9e6e77d08051f75b9f18bd97a2a
ZintleFaltein <zintlefaltein.zf@gmail.com>
2023-01-17 19:13:38 +0200

    Updated file names

--- Code/R code/Stomatal density v stomatal size.R
+++ Code/R code/Stomatal density v stomatal size.R
@@ -1,5 +1,5 @@
 # This script creates a scatter plot that explores the relationship
-# between stomatal density and size
+# between stomatal density, conductance and size
 
 library(ggplot2)
 library(ggpmisc)
@@ -8,10 +8,10 @@ library(tidyverse)
 library(readr)
 
 # load data
-stomatal_traits <- read_delim("C:/Users/User/Documents/GitHub/CO2-effects-on-geophytes/Data/Oxalis stomatal traits.csv", 
-                              delim = ";", escape_double = FALSE, trim_ws = TRUE)
+stomatal_traits <- read_delim('https://raw.githubusercontent.com/ZintleFaltein/CO2-effects-on-geophytes/master/Data/Oxalis%20stomatal%20traits.csv?token=GHSAT0AAAAAAB5TEBPFX3T7XH2VW7DBYHBCY6G2JZA',
+                              delim = ';')
 
-theme <- theme(panel.grid.minor = element_blank(),
+themed <- theme(panel.grid.minor = element_blank(),
                panel.grid.major = element_blank(),
                panel.background=element_rect(fill="white"),
                axis.line=element_line("black"),
@@ -39,7 +39,9 @@ stomatal_plot <- ggplot(stomatal_traits, aes(x = density, y = conductance, size 
   stat_poly_eq(formula = formula, aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")), 
                       parse = TRUE, size=3) +
   labs(x=expression(Stomatal~density~~(mm^-2)),y=expression(Stomatal~conductance~~(mu~mol~m^-2~s^-1))) +
-  theme 
+  themed 
+
+stomatal_plot
 
 #stomatal_plot <- stomatal_plot + scale_shape_manual(values = c(3, 16, 8)) + 
   #scale_size_manual(values = c(10, 10, 10))
@@ -57,31 +59,31 @@ stomatal_plot <- ggplot(stomatal_traits, aes(x = density, y = conductance, size 
 # #Strip at top of fig
 # stomatal_plot <-stomatal_plot + theme(strip.background=element_rect(fill="grey 80",colour="black"))
 # stomatal_plot <- stomatal_plot + theme(axis.ticks.x = element_blank(), axis.text.x = element_blank(), panel.spacing = unit(0.2, 'lines'))
-
-#Plot relationship between stomatal size and conductance
-formula = y ~ x
-density_conductance <- ggplot(stomatal_traits, aes(x = density, y = conductance))
-density_conductance <- density_conductance + facet_wrap(~Nutrients, )
-density_conductance <- density_conductance + geom_point() + scale_shape_manual(values = c(3, 16, 8)) + 
-  scale_size_manual(values = c(10, 10, 10)) +
- geom_smooth(data=stomatal_traits,method = "lm", formula = formula, size = 1,color="black",se=TRUE)
-#p <- p + scale_colour_manual(values = c("black", "red", 'blue'))
-# Show equation and R squared value on the plot
-density_conductance <- density_conductance + stat_poly_eq(formula = formula, aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")), parse = TRUE, size=3)
-density_conductance
-
-density_conductance <- density_conductance + labs(x=expression(Stomatal~density~~(mm^-2)),y=expression(Stomatal~conductance~~(mu~mol~m^-2~s^-1)))
-
-#Removes legend
-density_conductance <- density_conductance + guides(fill=FALSE)
-#Axis text size and direction
-density_conductance <- density_conductance + theme(text = element_text(size=10),axis.text.x = element_text(angle=0, vjust=1,color="black"),axis.text.y = element_text(color="black"))
-#Remove grid lines and set background white
-density_conductance <- density_conductance +theme(panel.grid.minor = element_blank(),panel.grid.major = element_blank(),panel.background=element_rect(fill="white"))
-#Black lines around panels
-density_conductance <- density_conductance + theme(axis.line=element_line("black"),panel.border=element_rect(fill=NA,colour="black"))
-#Strip at top of fig
-density_conductance <- density_conductance + theme(strip.background=element_rect(fill="grey 80",colour="black"))
-density_conductance <- density_conductance + theme(panel.spacing = unit(0.2, 'lines'))
-
-grid.arrange(stomatal_plot, density_conductance, ncol = 1)
+# 
+# #Plot relationship between stomatal size and conductance
+# formula = y ~ x
+# density_conductance <- ggplot(stomatal_traits, aes(x = density, y = conductance))
+# density_conductance <- density_conductance + facet_wrap(~Nutrients, )
+# density_conductance <- density_conductance + geom_point() + scale_shape_manual(values = c(3, 16, 8)) + 
+#   scale_size_manual(values = c(10, 10, 10)) +
+#  geom_smooth(data=stomatal_traits,method = "lm", formula = formula, size = 1,color="black",se=TRUE)
+# #p <- p + scale_colour_manual(values = c("black", "red", 'blue'))
+# # Show equation and R squared value on the plot
+# density_conductance <- density_conductance + stat_poly_eq(formula = formula, aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")), parse = TRUE, size=3)
+# density_conductance
+# 
+# density_conductance <- density_conductance + labs(x=expression(Stomatal~density~~(mm^-2)),y=expression(Stomatal~conductance~~(mu~mol~m^-2~s^-1)))
+# 
+# #Removes legend
+# density_conductance <- density_conductance + guides(fill=FALSE)
+# #Axis text size and direction
+# density_conductance <- density_conductance + theme(text = element_text(size=10),axis.text.x = element_text(angle=0, vjust=1,color="black"),axis.text.y = element_text(color="black"))
+# #Remove grid lines and set background white
+# density_conductance <- density_conductance +theme(panel.grid.minor = element_blank(),panel.grid.major = element_blank(),panel.background=element_rect(fill="white"))
+# #Black lines around panels
+# density_conductance <- density_conductance + theme(axis.line=element_line("black"),panel.border=element_rect(fill=NA,colour="black"))
+# #Strip at top of fig
+# density_conductance <- density_conductance + theme(strip.background=element_rect(fill="grey 80",colour="black"))
+# density_conductance <- density_conductance + theme(panel.spacing = unit(0.2, 'lines'))
+# 
+# grid.arrange(stomatal_plot, density_conductance, ncol = 1)
